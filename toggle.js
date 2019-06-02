5a464a881fc7b7a6e79893f144099d3e48a3fa32
Max Miracolo <max.miracolo@gmail.com>
2019-06-02 10:17:48 -0400

    add popup to allow toggling of the dark theme

---
+++ toggle.js
@@ -0,0 +1,16 @@
+function toggleDarkMode() {
+    const className = 'gh-dark-extension-wrapper';
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.contains(className)
+        ? body.classList.remove(className)
+        : body.classList.add(className);
+}
+
+chrome.runtime.onMessage.addListener(
+    function(message, sender, sendResponse) {
+        if(message.action === 'toggleDarkMode') {
+            toggleDarkMode();
+        }
+    });
+
+toggleDarkMode();
